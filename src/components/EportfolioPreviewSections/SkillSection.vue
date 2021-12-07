0eb134b7a2d940795d0a54975250ddb9f36e57db
amer <amer23zx@gmail.com>
2021-12-07 11:44:38 +0800

    Adding circle progress bar for slikks

--- src/components/EportfolioPreviewSections/SkillSection.vue
+++ src/components/EportfolioPreviewSections/SkillSection.vue
@@ -87,31 +87,67 @@
             <h3>Professional Skills</h3>
             <ul class="mh-professional-progress">
               <li>
-                <div
-                  class="mh-progress mh-progress-circle"
-                  data-progress="95"
-                ></div>
+                <div class="circle-wrap">
+                  <div class="circle">
+                    <div class="mask full">
+                      <div class="fill" style="transform: rotate(300deg)"></div>
+                    </div>
+
+                    <div class="mask half">
+                      <div class="fill"></div>
+                    </div>
+
+                    <div class="inside-circle">70%</div>
+                  </div>
+                </div>
                 <div class="pr-skill-name">Communication</div>
               </li>
               <li>
-                <div
-                  class="mh-progress mh-progress-circle"
-                  data-progress="55"
-                ></div>
+                <div class="circle-wrap">
+                  <div class="circle">
+                    <div class="mask full">
+                      <div class="fill" style="transform: rotate(250deg)"></div>
+                    </div>
+
+                    <div class="mask half">
+                      <div class="fill"></div>
+                    </div>
+
+                    <div class="inside-circle">30%</div>
+                  </div>
+                </div>
                 <div class="pr-skill-name">Team Work</div>
               </li>
               <li>
-                <div
-                  class="mh-progress mh-progress-circle"
-                  data-progress="86"
-                ></div>
+                <div class="circle-wrap">
+                  <div class="circle">
+                    <div class="mask full">
+                      <div class="fill" style="transform: rotate(150deg)"></div>
+                    </div>
+
+                    <div class="mask half">
+                      <div class="fill"></div>
+                    </div>
+
+                    <div class="inside-circle">20%</div>
+                  </div>
+                </div>
                 <div class="pr-skill-name">Project Management</div>
               </li>
               <li>
-                <div
-                  class="mh-progress mh-progress-circle"
-                  data-progress="60"
-                ></div>
+                <div class="circle-wrap">
+                  <div class="circle">
+                    <div class="mask full">
+                      <div class="fill" style="transform: rotate(126deg)"></div>
+                    </div>
+
+                    <div class="mask half">
+                      <div class="fill"></div>
+                    </div>
+
+                    <div class="inside-circle">60%</div>
+                  </div>
+                </div>
                 <div class="pr-skill-name">Creativity</div>
               </li>
             </ul>
@@ -121,3 +157,55 @@
     </div>
   </section>
 </template>
+
+<style scoped>
+.circle-wrap {
+  margin: 10px auto;
+  width: 150px;
+  height: 150px;
+  background: #e6e2e7;
+  border-radius: 50%;
+}
+.circle-wrap .circle .mask,
+.circle-wrap .circle .fill {
+  width: 150px;
+  height: 150px;
+  position: absolute;
+  border-radius: 50%;
+}
+.circle-wrap .circle .mask {
+  clip: rect(0px, 150px, 150px, 75px);
+}
+.circle-wrap .circle .mask .fill {
+  clip: rect(0px, 75px, 150px, 0px);
+  background-color: #61dfcb;
+}
+.circle-wrap .circle .mask.full,
+.circle-wrap .circle .fill {
+  animation: fill ease-in-out 3s;
+  /* transform: rotate(300deg); */
+}
+
+@keyframes fill {
+  0% {
+    transform: rotate(0deg);
+  }
+  /* 100% {
+    transform: rotate(126deg);
+  } */
+}
+.circle-wrap .inside-circle {
+  width: 130px;
+  height: 130px;
+  border-radius: 50%;
+  background: #fff;
+  line-height: 130px;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 10px;
+  position: absolute;
+  z-index: 100;
+  font-weight: 700;
+  font-size: 2em;
+}
+</style>
